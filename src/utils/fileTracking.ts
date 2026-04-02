c2766088c7827d317192fe3906e39d202c511df0
Matt Abrm <mattabraham20xx@gmail.com>
2026-04-02 17:24:14 -0400

    Fixes Up makeChunks function inside fileTracking.ts and made some changes to schema.sql to ensure values are not null when inserting into database. Also adds some FK relations to ensure db stays consitant.

--- src/utils/fileTracking.ts
+++ src/utils/fileTracking.ts
@@ -1,5 +1,20 @@
 import * as vscode from 'vscode';
 
+export class chunk {
+    chunkId: number | null = null;
+    contents: string = "";
+    startpos: number | null = null;
+    endpos: number | null = null;
+
+    constructor(chunkId: number, contents: string, startpos: number, endpos: number){
+        this.chunkId = chunkId;
+        this.contents = contents;
+        this.startpos = startpos;
+        this.endpos = endpos;
+    }
+}
+
+
 
 export async function getFileContents() {
     const activeEditor = vscode.window.activeTextEditor;
@@ -11,4 +26,28 @@ export async function getFileContents() {
 
     return document.getText();
         
+}
+
+
+
+
+export async function makeChunks(): Promise<chunk[] | undefined> {
+    const content = await getFileContents();
+
+    if (!content) {
+        return undefined;
+    }
+
+    const chunkSize = 500;
+    const numOfChunks = Math.ceil(content.length / chunkSize);
+    let currentpos = 0;
+    let chunks: chunk[] = [];
+
+    for (let i = 0; i < numOfChunks; i++) {
+        const chunkContent = content.slice(currentpos, currentpos + chunkSize);
+        chunks.push(new chunk(i, chunkContent, currentpos, currentpos + chunkSize));
+        currentpos += chunkSize;
+    }
+
+    return chunks;
 }

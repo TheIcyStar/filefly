c04d40369673ee1d43ad3509cb9f01b50f0ed35d
EarthCow <56940983+EarthCow@users.noreply.github.com>
2026-04-09 13:37:50 -0400

    Get the relative path and filename

--- src/listeners/fileCreateListener.ts
+++ src/listeners/fileCreateListener.ts
@@ -1,5 +1,22 @@
 import * as vscode from 'vscode'
+import * as fs from 'fs'
+import * as path from "path";
 
 export function fileCreateListener(fileCreateEvent: vscode.FileCreateEvent) {
     vscode.window.showInformationMessage(`You created file(s): "${fileCreateEvent.files}!"`)
+    for (let i = 0; i < fileCreateEvent.files.length; i++) {
+        const uri = fileCreateEvent.files[i];
+        const filePath = uri.fsPath;
+        try {
+            if (!fs.existsSync(filePath)) {
+                continue;
+            }
+            const filename = path.parse(filePath).base;
+            const relativePath = vscode.workspace.asRelativePath(uri, false);
+            vscode.window.showInformationMessage(`Path: ${relativePath}, Filename: ${filename}!`)
+        } catch (error) {
+            vscode.window.showErrorMessage(`Failed to get data from ${filePath}`)
+            console.error(`Failed to get file info for ${filePath}:`, error);
+        }
+    }
 }
